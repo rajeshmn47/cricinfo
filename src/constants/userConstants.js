f4e08793e5a1a184688f2abd659d85686d77de4e
rajeshmn47 <rajeshmn47@gmail.com>
2025-10-18 06:54:02 +0530

    fix

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -66,8 +66,8 @@ function geturl() {
         // return "https://dreamelevenclonebackend-g4iw.onrender.com";
         // return "https://fantasycricket4u.com";
         // return "https://backend.dreambet11.io";
-        // return "https://gamizo.duckdns.org";
-        return "http://localhost:8000"
+        return "https://gclteam.duckdns.org";
+        // return "http://localhost:8000"
     }
     // return "https://dreamelevenclonebackend-7l34.onrender.com";
     // return "https://dreamelevenclonebackend-1v8e.onrender.com";
@@ -77,12 +77,12 @@ function geturl() {
     // return "http://dream-env.eba-6d6ds7up.us-east-1.elasticbeanstalk.com";
     // return "http://54.172.255.164";
     // return 'https://apk.thepowerplay.co';
-    return 'http://localhost:8000';
+    // return 'http://localhost:8000';
     // return 'http://46.28.44.4:8000';
     // return "https://fantasycricket4u.com";
     // return "https://dreamelevenclonebackend-g4iw.onrender.com"
     // return "https://backend.dreambet11.io";
-    // return "https://fango11.duckdns.org";
+    return "https://gclteam.duckdns.org";
 }
 
 function getfrontendurl() {
